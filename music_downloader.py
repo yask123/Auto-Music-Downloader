21286d1d33cb94d8139c3a9bf3ec7de99b20dd05
sdubrul <stijndubrul@gmail.com>
2015-09-30 00:39:24 +0200

    removed unused import and changed all mixed indents to 4 spaces

--- music_downloader.py
+++ music_downloader.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
 import os
-import glob
 from bs4 import BeautifulSoup
 
-# Version compatiblity
+# Version compatibility
 import sys
-if (sys.version_info > (3,0)):
-	from urllib.request import urlopen
-	from urllib.parse import quote_plus as qp
-	raw_input = input
+if(sys.version_info > (3, 0)):
+    from urllib.request import urlopen
+    from urllib.parse import quote_plus as qp
+    raw_input = input
 else:
-	from urllib2 import urlopen
-	from urllib import quote_plus as qp
+    from urllib2 import urlopen
+    from urllib import quote_plus as qp
 
 search = ''
 # We do not want to accept empty inputs :)
 while search == '':
-  search = raw_input('Enter songname/ lyrics/ artist.. or whatever\n> ')
+    search = raw_input('Enter songname/ lyrics/ artist.. or whatever\n> ')
 search = qp(search)
 
 print('Making a Query Request! ')
@@ -28,15 +27,16 @@ html = response.read()
 soup = BeautifulSoup(html, 'html.parser')
 for link in soup.find_all('a'):
     if '/watch?v=' in link.get('href'):
-    	print(link.get('href'))
-    	# May change when Youtube gets updated in the future.
-    	video_link = link.get('href')
-    	break
+        print(link.get('href'))
+        # May change when Youtube gets updated in the future.
+        video_link = link.get('href')
+        break
 
 # Links are relative on page, making them absolute.
 video_link = 'http://www.youtube.com/' + video_link
-command = 'youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 ' + video_link
+dl_cmd = 'youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 '
+command = dl_cmd + ' ' + video_link
 
 # Youtube-dl is a proof that god exists.
-print ('Downloading...')
+print('Downloading...')
 os.system(command)
