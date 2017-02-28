57b721b03276baca7982c52bf9d5f1bd451c01c0
rachmadaniHaryono <foreturiga@gmail.com>
2017-02-28 08:51:22 +0800

    chg: dev: merge setup and config

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """setup file."""
+from sys import platform
 try:
     from setuptools import setup
 except ImportError:
@@ -14,18 +15,28 @@ except ImportError:
     read_md = lambda f: open(f, 'r').read()  # NOQA
 
 setup(
-    name='Instant Music Downloader',
-    version='1.0',
-    description='Downloads Music From The Web',
+    name='instantmusic',
+    version='1.2',
+    description='Instantly download any song! Without knowing the name of the song!!!!',
     long_description=read_md('README.md'),
-    author='yask123',
+    author='Yask Srivastava',
     author_email='yask123@gmail.com',
     url='https://github.com/yask123/Instant-Music-Downloader',
+    license='MIT',
+    packages=['instantmusic'],
+    scripts=['bin/instantmusic'],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-    ]
+    ],
+    install_requires=[
+        'youtube-dl',
+        'BeautifulSoup4',
+        'eyed3',
+        'requests'
+    ] + (['pyreadline'] if platform.startswith('win') else []),
+    zip_safe=False,
 )
